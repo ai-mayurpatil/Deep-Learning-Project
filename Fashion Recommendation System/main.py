b5d54d45c953b56f833486dc1b6fcd02db7c957c
ai-mayurpatil <ai.mayurpatil@gmail.com>
2023-04-07 20:37:47 +0530

    streamlit app created and test

---
+++ Fashion Recommendation System/main.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import os
+from PIL import Image
+
+st.title('Fashion recommender system')
+
+def save_uploaded_file(uploaded_file):
+    try:
+        with open(os.path.join('uploads',uploaded_file.name),'wb') as f:
+            f.write(uploaded_file.getbuffer())
+        return 1
+    except:
+        return 0
+
+#step
+#file upload -> save
+uploaded_file = st.file_uploader("Choose an image")
+if uploaded_file is not None:
+    if save_uploaded_file(uploaded_file):
+        #display the file
+        display_image = Image.open(uploaded_file)
+        st.image(display_image)
+    else:
+        st.header("Some error occured")
